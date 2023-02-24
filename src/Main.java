ed93053afbd30fa01624cabcc93e992935a1e405
amature.build <amature.build@gmail.com>
2023-02-24 01:36:14 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String msg1 = "Enter the Starting value";
+        String msg2 = "Enter the Ending value";
+
+
+    }
+}
